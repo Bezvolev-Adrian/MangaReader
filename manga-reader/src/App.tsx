4662dc435fbbca0d304d924052926100a29af240
AdrianBezvolev <bezvolevadrian.i@gmail.com>
2024-07-07 16:41:18 +0300

    reinstalled project

---
+++ manga-reader/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "@mui/material/Button";
+import BasicButton from "./components/BasicButton";
+
+function App() {
+  return <BasicButton />;
+}
+
+export default App;
